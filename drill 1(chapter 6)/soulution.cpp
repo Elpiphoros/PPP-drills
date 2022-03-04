74660b2ce7a6838ae018a4d088b357c9d0e2cda0
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-03-04 17:06:12 +0100

    Create soulution.cpp

---
+++ drill 1(chapter 6)/soulution.cpp
@@ -0,0 +1,8 @@
+/*task:
+1. Change the character used as the exit command from q to x.
+2. Change the character used as the print command from ; to =.
+3. Add a greeting line in main():
+"Welcome to our simple calculator.
+Please enter expressions using floating-point numbers."
+4. Improve that greeting by mentioning which operators are available and how to print and exit.
+*/
