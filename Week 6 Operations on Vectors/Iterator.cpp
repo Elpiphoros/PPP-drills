98962921617ab6883d4432b05c75bb274350f551
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-03-16 14:02:04 +0100

    Create Iterator.cpp

---
+++ Week 6 Operations on Vectors/Iterator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> num{1, 2, 3, 4, 5};
+    vector<int> ::iterator item;
+    cout << "Initial Element: ";
+    
+    for(item = num.begin(); item != num.end(); item++)
+    {
+        cout << *item << " ";
+    }
+    
+    return 0;
+}
