10922b423ef877de5d1512f5eefd301390f8b741
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-03-16 13:21:11 +0100

    Update add_access_change_remove.cpp

--- Week 6 Operations on Vectors/add_access_change_remove.cpp
+++ Week 6 Operations on Vectors/add_access_change_remove.cpp
@@ -50,3 +50,45 @@ int main()
     
     return 0;
 }
+
+/*
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> num{1, 2, 3, 4, 5};
+    vector<int> ::iterator item;
+    cout << "Initial Element: ";
+    
+    for(item = num.begin(); item != num.end(); item++)
+    {
+        cout << *item << " ";
+    }
+    
+    return 0;
+}
+*/#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(&a, &b);
+    cout << "a: " << a << "b: " << *b;
+    
+    
+    return 0;
+}
