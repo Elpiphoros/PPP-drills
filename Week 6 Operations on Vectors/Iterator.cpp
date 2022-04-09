cb6a2a5b027b463c53f9c0fbde331fa84a44ad35
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-09 15:53:20 +0200

    Update Iterator.cpp

--- Week 6 Operations on Vectors/Iterator.cpp
+++ Week 6 Operations on Vectors/Iterator.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     vector<int> num{1, 2, 3, 4, 5};
-    vector<int> ::iterator item;
+    vector<int> :: iterator item;
     cout << "Initial Element: ";
     
     for(item = num.begin(); item != num.end(); item++)
