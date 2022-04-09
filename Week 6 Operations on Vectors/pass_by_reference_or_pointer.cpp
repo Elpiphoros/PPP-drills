1524533c62f0bd9aec33c1195bbf692c1e15da6d
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-09 16:19:03 +0200

    Update and rename pass_by_reference.cpp to pass_by_reference_or_pointer.cpp

--- Week 6 Operations on Vectors/pass_by_reference_or_pointer.cpp
+++ Week 6 Operations on Vectors/pass_by_reference_or_pointer.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//passing by pointer
 void swap(int *a, int *b)
 {
     int temp;
@@ -16,7 +17,23 @@ int main()
     int a = 10, b = 20;
     swap(&a, &b);
     cout << "a: " << a << " " << "b: " << b;
-    
-    
+     
     return 0;
 }
+/*// passing by reference
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(a, b);
+    cout << "a: " << a << " " << "b: " << b;
+    
+    return 0;
+}*/
