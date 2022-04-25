ead28a948841a1c21da8566ecb22282335323e88
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-26 01:22:48 +0200

    Update solution.cpp

--- drill 2(chapter 7)/solution.cpp
+++ drill 2(chapter 7)/solution.cpp
@@ -26,7 +26,7 @@ class Token
 class Token_stream
 {
 	public:
-		Token_stream() :full(0), buffer(0) { }
+		Token_stream() :full(0), buffer(0) {}
 		Token get();
 		void unget(Token t) { buffer = t; full = true; }
 		void ignore(char); // discard characters up to and including a c
