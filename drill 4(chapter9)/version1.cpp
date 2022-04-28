c7f4789f2f59c50206a1c7b0fbc988fe4f632c55
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-28 23:00:18 +0200

    Update version1.cpp

--- drill 4(chapter9)/version1.cpp
+++ drill 4(chapter9)/version1.cpp
@@ -36,20 +36,20 @@ void add_day(Date& date, int n)
        
 /*void f()
 {
-     Date today3;
-          today3.y = 2009
-          today3.m = 3
-          today3.d = 3
-     cout << today << '\n';
+    Date today3;
+        today3.y = 2009
+        today3.m = 3
+        today3.d = 3
+    cout << today << '\n';
     
-     init_day(today3, 2009, 3, 3);
+    init_day(today3, 2009, 3, 3);
      
-     Date tomorrow;
-          tomorrow.y = today3.y;
-          tomorrow.m = today3.m;
-          tomorrow.d = today3.d + 1;
-     cout << tomorrow << '\n';
-     add_day(today3, 1);
+    Date tomorrow;
+        tomorrow.y = today3.y;
+        tomorrow.m = today3.m;
+        tomorrow.d = today3.d + 1;
+    cout << tomorrow << '\n';
+    add_day(today3, 1);
 }*/
 int main()
 {
@@ -62,14 +62,14 @@ int main()
     today.d = 12;
     
     Date x;
-         x.y = -1;
-         x.m = 13;
-         x.d = 32;
+        x.y = -1;
+        x.m = 13;
+        x.d = 32;
     
     Date y;
-         y.y = 2000;
-         y.m = 2;
-         y.d = 29;
+        y.y = 2000;
+        y.m = 2;
+        y.d = 29;
 
     cout << today.y << "." << today.m << "." << today.d << "."<< endl;  
     
@@ -84,5 +84,5 @@ int main()
     add_day(today2, 1);
     cout << today2.y << "." << today2.m << "." << today2.d << endl;
      
-return 0;
+    return 0;
 }
