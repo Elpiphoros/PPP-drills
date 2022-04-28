8fc0de27fd4b6cb064674c9c90519d750b6dec63
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-28 08:06:12 +0200

    Update solution.cpp

--- drill 2(chapter 7)/solution.cpp
+++ drill 2(chapter 7)/solution.cpp
@@ -79,7 +79,6 @@ class Token
 	double value;
 	string name;
 	// three constructors
-	Token() : kind(0) {}
 	Token(char ch) :kind(ch), value(0) {}
 	Token(char ch, double val) :kind(ch), value(val) {}
 	Token(char ch, string n) : kind(ch), name(n) {}
