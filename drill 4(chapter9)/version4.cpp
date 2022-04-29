920df76f2fa0796e295334e372a836a1fb78aa1b
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-29 20:23:02 +0200

    Update version4.cpp

--- drill 4(chapter9)/version4.cpp
+++ drill 4(chapter9)/version4.cpp
@@ -18,5 +18,20 @@ const vector<string> months =
 
 enum class Month
 {
-    jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec
+    jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
 };
+
+/*An enum (an enumeration) is a very simple user-defined type, specifying its set of values (its enumerators) as symbolic constants. For example:
+emu class Month
+{
+    jan = "January", feb = "February", mar = "March",
+    apr = "April", may = "May", jun = "June",
+    jul = "July", aug = "August", sep = "September",
+    oct = "October", nov = "November", dec = "December"
+};
+The “body” of an enumeration is simply a list of its enumerators. 
+The class in enum class means that the enumerators are in the scope of the enumeration. That is, to refer to jan, we have to say Month::jan.
+eg: Month m = Month::jan;
+    Month m2 = feb;  // error: feb is not in scope
+    string n = m;    // error: can’t assign a Month to a string
+*/
