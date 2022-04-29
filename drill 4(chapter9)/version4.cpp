2861279e156ffa02dfc56ab3b452b53ec200a508
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-29 20:27:01 +0200

    Update version4.cpp

--- drill 4(chapter9)/version4.cpp
+++ drill 4(chapter9)/version4.cpp
@@ -34,4 +34,15 @@ The class in enum class means that the enumerators are in the scope of the enume
 eg: Month m = Month::jan;
     Month m2 = feb;  // error: feb is not in scope
     string n = m;    // error: can’t assign a Month to a string
+    
+enum Month  // note: no “class”!!!
+{
+    jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
+    Month m = feb; // OK: feb in scope!!!
+    Month m2 = Month::feb; // also OK
+    m = 7; // error: can’t assign an int to a Month
+    int n = m; // OK: we can assign a Month to an int
+    
+    “plain” enums are less strict than enum classes!!
 */
