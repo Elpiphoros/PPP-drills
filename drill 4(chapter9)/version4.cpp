e3d8f53ab6b3f2f4aae5ac6846eac8f4f46d751b
Yoko <99318935+Elpiphoros@users.noreply.github.com>
2022-04-29 20:12:54 +0200

    Update version4.cpp

--- drill 4(chapter9)/version4.cpp
+++ drill 4(chapter9)/version4.cpp
@@ -2,20 +2,21 @@
 
 const vector<string> months = 
 {
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 };
 
-enum class Month {
-	jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec
+enum class Month
+{
+    jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec
 };
